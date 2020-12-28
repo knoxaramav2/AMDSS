560b998541767235e1a7048a2ca9a2ad5895ecc3
U-DESKTOP-2LM261U\admin <knoxarama42>
2020-12-27 21:16:06 -0600

    Database models, view stubs

---
+++ AMDSS/AMDSS/Models/UserData.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMDSS.Models
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; } //Salt/Hash
+        public int PhotoId { get; set; }
+    }
+
+    public class AdminData
+    {
+        public int Id { get; set; }
+
+        public UserData AdminUser { get; set; }
+    }
+
+    public class AccountDB : DbContext
+    {
+        public AccountDB(DbContextOptions<AccountDB> options) : base (options)
+        {
+
+        }
+
+        public DbSet<UserData> Users { get; set; }
+        public DbSet<AdminData> Admins { get; set; }
+    }
+}
